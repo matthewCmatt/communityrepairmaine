1af04d3dc0a31bb15c2f18d890e8d5aca128a9a6
Matt Carroll <mc@qacafe.com>
2026-04-02 22:26:41 -0400

    add go backend with Containerfile

---
+++ pb/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	_ "mendmaine/migrations"
+
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	"github.com/pocketbase/pocketbase/tools/osutils"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+		return se.Next()
+	})
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Dashboard
+		// (the IsProbablyGoRun check is to enable it only during development)
+		Automigrate: osutils.IsProbablyGoRun(),
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}

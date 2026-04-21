c3410a7982d6a61cd42b0008c8753b4e8f0f64d7
Matt Carroll <matthewcmatt@gmail.com>
2026-04-20 21:14:11 -0400

    use correct type for PageServerLoad

--- web/src/routes/events/[id]/+page.server.ts
+++ web/src/routes/events/[id]/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageServerLoad } from './$types';
 import { pb } from '$lib/pb.server';
 import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	let event = await pb.collection('events').getOne(params.id);

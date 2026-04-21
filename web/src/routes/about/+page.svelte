0b32857045e9c0010b84ec93b71b75131cdd2eab
Matt Carroll <matthewcmatt@gmail.com>
2026-04-20 22:24:26 -0400

    add bit about Maine

--- web/src/routes/about/+page.svelte
+++ web/src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <p>This is a site where you can find free local repair meetups.</p>
 
+<p>This applies across the state of Maine</p>
+
 <p>
 	The source code for this website is <a
 		href="https://github.com/matthewCmatt/communityrepairmaine"

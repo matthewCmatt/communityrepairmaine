574196c9e18d4dfe2009b8f815eeb26049c006d2
Matt Carroll <matthewcmatt@gmail.com>
2026-09-12 22:35:48 -0400

    reduce size a bit

--- web/src/lib/components/EventMapIcon.svelte
+++ web/src/lib/components/EventMapIcon.svelte
@@ -8,8 +8,8 @@
 <style>
 	a {
 	    display: inline-block;
-		width: 40px;
-		height: 40px;
+		width: 30px;
+		height: 30px;
 		background-color: #823038; /* Svelte orange */
 		border: 2px solid #fff;
 		border-radius: 50% 50% 50% 0;
